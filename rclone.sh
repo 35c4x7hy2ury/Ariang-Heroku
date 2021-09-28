e7ffa2dc559b0729ee76d08100c710952dd0ade8
35c4x7hy2ury <91568183+35c4x7hy2ury@users.noreply.github.com>
2021-09-28 21:41:02 +0100

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -2,6 +2,7 @@
 
 if [ "$ENABLE_RCLONE" = "true" ]; then
   echo "Start Rclone, please make sure you can connect to Github website. if not, please set docker env ENABLE_RCLONE=false"
+  /usr/bin/rclone --conf-path="/app/conf/rclone.conf" \
   rclone rcd --rc-web-gui \
     --rc-web-gui-no-open-browser \
     --rc-addr :5572 \
@@ -9,6 +10,8 @@ if [ "$ENABLE_RCLONE" = "true" ]; then
     --rc-pass $ARIA2_PWD \
     --cache-dir /app/.cache
 else
+  echo "Start aria2 with standard mode" 
+  /usr/bin/rclone --conf-path="/app/conf/rclone.conf"
   echo "Skip starting Rclone as it's been disabled"
   sleep 3650d
 fi
